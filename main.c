19c014c6d9caadd421dc62ab642349f1ab0e6c2c
Jan Horacek <me@apophis.cz>
2020-11-01 11:01:36 +0100

    Make code compilable.

--- main.c
+++ main.c
@@ -18,24 +18,24 @@ int main() {
 	init();
 
 	while (true) {
-		wdt_reset();
+		// wdt_reset();
 	}
 }
 
 static inline void init() {
 	ACSR |= ACD;  // analog comparator disable
-	WDTCR |= 1 << WDE;  // watchdog enable
-	WDTCR |= WDP2; // ~250 ms timeout
+	// WDTCR |= 1 << WDE;  // watchdog enable
+	// WDTCR |= WDP2; // ~250 ms timeout
 
 	// Setup timer 0
 	TCCR0A |= 1 << WGM01; // CTC mode
 	TCCR0B |= 1 << CS01; // no prescaler
-	TIMSK |= 1 << OCIE0A; // enable compare match A
+	TIMSK0 |= 1 << OCIE0A; // enable compare match A
 	OCR0A = 99;
 
 	sei(); // enable interrupts globally
 }
 
-ISR(TIM0_COMPA_vect) {
+ISR(TIMER0_COMPA_vect) {
 	// Timer 0 @ 10 kHz (period 100 us)
 }

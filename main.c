19b8d19ac2cc7acf7d1f04a605fc37ba910d11cd
Jan Horacek <me@apophis.cz>
2020-11-01 13:28:10 +0100

    Add IO defs.

--- main.c
+++ main.c
@@ -19,7 +19,8 @@ typedef enum {
 typedef struct {
 	uint8_t pinc_pot_mask;
 	uint8_t pinb_led_mask;
-	uint8_t servo_pin; // TODO
+	uint8_t pind_servo_mask;
+	uint8_t pinb_servo_mask;
 	uint8_t pind_btn_mask;
 
 	TurnoutPos position; // TODO: reset on init
@@ -29,7 +30,39 @@ typedef struct {
 } Turnout;
 
 #define TURNOUTS_COUNT 4
-Turnout turnouts[TURNOUTS_COUNT];
+Turnout turnouts[TURNOUTS_COUNT] = {
+	{ // servo 1 [0]
+		.pinc_pot_mask = 1 << PC0,
+		.pinb_led_mask = 1 << PB4,
+		.pind_servo_mask = 0,
+		.pinb_servo_mask = 1 << PB0,
+		.pind_btn_mask = 1 << PD4,
+	},
+
+	{ // servo 2 [1]
+		.pinc_pot_mask = 1 << PC1,
+		.pinb_led_mask = 1 << PB3,
+		.pind_servo_mask = 1 << PD7,
+		.pinb_servo_mask = 0,
+		.pind_btn_mask = 1 << PD3,
+	},
+
+	{ // servo 3 [2]
+		.pinc_pot_mask = 1 << PC2,
+		.pinb_led_mask = 1 << PB1,
+		.pind_servo_mask = 1 << PD6,
+		.pinb_servo_mask = 0,
+		.pind_btn_mask = 1 << PD1,
+	},
+
+	{ // servo 4 [3]
+		.pinc_pot_mask = 1 << PC3,
+		.pinb_led_mask = 1 << PB2,
+		.pind_servo_mask = 1 << PD5,
+		.pinb_servo_mask = 0,
+		.pind_btn_mask = 1 << PD2,
+	},
+};
 
 #define DEBOUNCE_READS 20 // also time in ms
 
